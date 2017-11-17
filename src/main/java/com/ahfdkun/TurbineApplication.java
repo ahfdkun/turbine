83a7fc9c7c9e2ef908631605b1400ccf4441caf2
zhaoyakun <yakun.zhao@derbysoft.com>
2017-11-17 20:33:07 +0800

    turbine

--- src/main/java/com/ahfdkun/TurbineApplication.java
+++ src/main/java/com/ahfdkun/TurbineApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
-@SpringBootApplication	
+@SpringBootApplication
 @EnableTurbine
 @EnableDiscoveryClient
 public class TurbineApplication {
